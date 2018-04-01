0463cb84dee1b00e188b95348458f109c4238de6
Steve Watkins <space_monkey14@hotmail.co.uk>
2018-04-01 10:33:26 +0100

    added post create, edit and delete functionality

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -13,3 +15,21 @@ class BlogDetailView(DetailView):
 
 	model = Post
 	template_name = 'blog/post_detail.html'
+
+class BlogCreateView(CreateView):
+
+	model = Post
+	template_name = 'blog/post_new.html'
+	fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+
+	model = Post
+	template_name = 'blog/post_edit.html'
+	fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+
+	model = Post
+	template_name = 'blog/post_delete.html'
+	success_url = reverse_lazy('home')
